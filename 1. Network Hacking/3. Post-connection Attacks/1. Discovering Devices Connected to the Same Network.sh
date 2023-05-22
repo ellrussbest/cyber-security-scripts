2eb88d7d17ba072637536455803fa3708372d06b
ellrussbest <ellrussbest@gmail.com>
2023-05-22 09:00:47 +0200

    added new changes

--- 1. Network Hacking/3. Post-connection Attacks/1. Discovering Devices Connected to the Same Network.sh
+++ 1. Network Hacking/3. Post-connection Attacks/1. Discovering Devices Connected to the Same Network.sh
@@ -26,5 +26,18 @@ netdiscover -r 10.0.2.1/24
     5. computer names
     6. bypass security
     7. bypass firewalls
+    8. We can also use NMAP to gain access using code execution vulnerabilities
 zenmap is the GUI of nmap
 '
+
+:'
+- fun fact:
+    - by default iOS devices do not have an ssh server.
+    - usually when you jailbreak the phone, it will automatically install
+    - an ssh server. And the password of that server is set to alpine
+    - so it is always worth a try to connect with a vulnerable phone like this with
+    - the default password.
+    e.g.
+'
+
+ssh root@192.168.1.12 
